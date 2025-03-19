db8aa423c6570451861ab9b0edce6e924b5d2c45
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-03-19 23:41:55 +0900

    WIP

--- scilab_optimization/cmake/FindScilab.cmake
+++ scilab_optimization/cmake/FindScilab.cmake
@@ -4,22 +4,15 @@
 # This module defines:
 #  SCILAB_ROOT: Scilab installation path
 #  SCILAB_BINARY: Scilab binary path
-#  SCILAB_MEX_INCLUDE_DIR: include path for mex.h (optional)
-#  SCILAB_CORE_INCLUDE_DIR: main headers (optional)
-#  SCILAB_MEX_LIBRARY: path to libmex.so (optional)
-#  SCILAB_MX_LIBRARY:  path to libmx.so (optional)
-#  SCILAB_SCICORE_LIBRARY: path to scicore (optional)
-#  SCILAB_LIBRARIES:   list of required and optional libraries
+#  SCILAB_LIBRARIES: list of required and optional libraries
 #
 # Copyright (c) 2009 Arnaud Barré <arnaud.barre@gmail.com>
 # Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if(SCILAB_ROOT AND SCILAB_LIBRARIES)
   set(Scilab_FIND_QUIETLY TRUE)
 endif()
 
-# platform-specific paths
 if(WIN32)
   get_filename_component(
     SCILAB_VER
@@ -31,17 +24,9 @@ if(WIN32)
     "etc/scilab.start"
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scilab\\${SCILAB_VER};SCIPATH]"
   )
-  set(SCILAB_MODULE_PATHS "${SCILAB_ROOT}/modules")
   set(SCILAB_LIBRARIES_PATHS "${SCILAB_ROOT}/bin")
-  set(SCILAB_MODULES_MEXLIB_PATHS "${SCILAB_MODULE_PATHS}/mexlib/includes")
-  set(SCILAB_MODULES_CORE_PATHS "${SCILAB_MODULE_PATHS}/core/includes")
-  set(SCILAB_BINARY_PATHS "${SCILAB_ROOT}/bin")
-  set(LIBMEX "libmex.dll")
-  set(LIBMX "libmx.dll")
   set(LIBSCILAB "LibScilab")
-  set(LIBSCICORE "")
   set(SCILAB_BIN "Scilex")
-  set(SCILAB_SCICORE_LIBRARY "NOTUSED")
 
 elseif(APPLE)
   file(
@@ -54,76 +39,28 @@ elseif(APPLE)
     "Contents/MacOS/share/scilab/etc/scilab.start"
     ${SCILAB_PATHS}
   )
-  set(SCILAB_MODULE_PATHS "${SCILAB_ROOT}/Contents/MacOS/share/scilab/modules")
   set(SCILAB_LIBRARIES_PATHS "${SCILAB_ROOT}/Contents/MacOS/lib/scilab")
-  set(SCILAB_MODULES_MEXLIB_PATHS "${SCILAB_ROOT}/Contents/MacOS/include/scilab")
-  set(SCILAB_MODULES_CORE_PATHS "${SCILAB_ROOT}/Contents/MacOS/include/scilab")
-  set(SCILAB_BINARY_PATHS "${SCILAB_ROOT}/Contents/MacOS/bin")
-  set(LIBMEX "libmex.dylib")
-  set(LIBMX "libmx.dylib")
   set(LIBSCILAB "scilab")
-  set(LIBSCICORE "scicore")
   set(SCILAB_BIN "scilab")
 
 else()
-  set(SCILAB_PATHS
-      "/usr/share/scilab"
-      "/usr/local/share/scilab"
-      "/opt/local/share/scilab"
-  )
-  set(SCILAB_MODULE_PATHS
-      "/usr/include/scilab"
-      "/usr/local/include/scilab"
-      "/opt/local/include/scilab"
-  )
   set(SCILAB_LIBRARIES_PATHS
       "/usr/lib/x86_64-linux-gnu/scilab"
       "/usr/lib/scilab"
       "/usr/local/lib/scilab"
-      "/opt/local/lib/scilab"
   )
-  set(SCILAB_MODULES_MEXLIB_PATHS
-      "/usr/include/scilab/mexlib"
-      "/usr/local/include/scilab/mexlib"
-      "/opt/local/include/scilab/mexlib"
-  )
-  set(SCILAB_MODULES_CORE_PATHS
-      "/usr/include/scilab/core"
-      "/usr/local/include/scilab/core"
-      "/opt/local/include/scilab/core"
-  )
-  set(SCILAB_BINARY_PATHS
-      "/usr/bin"
-      "/usr/local/bin"
-      "/opt/local/bin"
-  )
-
   find_path(
     SCILAB_ROOT
     "etc/scilab.start"
-    ${SCILAB_PATHS}
+    "/usr/share/scilab"
+    "/usr/local/share/scilab"
   )
-
   set(LIBSCILAB "scilab-cli")
-  set(LIBSCICORE "scicore")
-  set(LIBMEX "libmex.so")
-  set(LIBMX "libmx.so")
   set(SCILAB_BIN "scilab-cli")
 endif()
 
-find_program(
-  SCILAB_BINARY
-  ${SCILAB_BIN}
-  ${SCILAB_BINARY_PATHS}
-)
-find_library(
-  SCILAB_SCILAB_LIBRARY
-  ${LIBSCILAB}
-  ${SCILAB_LIBRARIES_PATHS}
-  NO_DEFAULT_PATH
-)
+find_program(SCILAB_BINARY ${SCILAB_BIN} PATHS "/usr/bin" "/usr/local/bin")
 
-# Optional libraries (linked in order)
 macro(
   _find_optional_lib
   var
@@ -140,33 +77,23 @@ macro(
   endif()
 endmacro()
 
+find_library(
+  SCILAB_SCILAB_LIBRARY ${LIBSCILAB}
+  PATHS ${SCILAB_LIBRARIES_PATHS}
+  NO_DEFAULT_PATH
+)
 _find_optional_lib(SCILAB_CALL_SCILAB_LIBRARY scicall_scilab)
 _find_optional_lib(SCILAB_CONSOLE_MINIMAL_LIBRARY sciconsole-minimal)
 _find_optional_lib(SCILAB_COMPLETION_LIBRARY scicompletion)
 _find_optional_lib(SCILAB_COMMONS_LIBRARY scicommons)
 _find_optional_lib(SCILAB_LOCALIZATION_LIBRARY scilocalization)
 _find_optional_lib(SCILAB_CLI_LIBRARY scilab-cli)
-_find_optional_lib(SCILAB_MEX_LIBRARY ${LIBMEX})
-_find_optional_lib(SCILAB_MX_LIBRARY ${LIBMX})
-_find_optional_lib(SCILAB_SCICORE_LIBRARY ${LIBSCICORE})
+_find_optional_lib(SCILAB_MEX_LIBRARY mex)
+_find_optional_lib(SCILAB_MX_LIBRARY mx)
+_find_optional_lib(SCILAB_SCICORE_LIBRARY scicore)
 
-find_path(
-  SCILAB_MEX_INCLUDE_DIR
-  "mex.h"
-  ${SCILAB_MODULES_MEXLIB_PATHS}
-  "${SCILAB_MODULES_MEXLIB_PATHS}/mexlib"
-  NO_DEFAULT_PATH
-)
-find_path(
-  SCILAB_CORE_INCLUDE_DIR
-  "core_math.h"
-  ${SCILAB_MODULES_CORE_PATHS}
-  "${SCILAB_MODULES_CORE_PATHS}/core"
-  NO_DEFAULT_PATH
-)
-
-# Compose SCILAB_LIBRARIES
-set(SCILAB_LIBRARIES ${SCILAB_SCILAB_LIBRARY})
+# 正しいリンク順序でライブラリを構成
+set(SCILAB_LIBRARIES "")
 foreach(
   lib
   SCILAB_CALL_SCILAB_LIBRARY
@@ -175,11 +102,7 @@ foreach(
   SCILAB_COMMONS_LIBRARY
   SCILAB_LOCALIZATION_LIBRARY
   SCILAB_CLI_LIBRARY
-  SCILAB_MEX_LIBRARY
-  SCILAB_MX_LIBRARY
-  SCILAB_SCICORE_LIBRARY
-  SCILAB_API_LIBRARY
-  SCILAB_TYPES_LIBRARY
+  SCILAB_SCILAB_LIBRARY
 )
   if(${lib})
     list(
@@ -203,11 +126,6 @@ endif()
 
 mark_as_advanced(
   SCILAB_SCILAB_LIBRARY
-  SCILAB_SCICORE_LIBRARY
-  SCILAB_MEX_LIBRARY
-  SCILAB_MX_LIBRARY
   SCILAB_BINARY
   SCILAB_LIBRARIES
-  SCILAB_MEX_INCLUDE_DIR
-  SCILAB_CORE_INCLUDE_DIR
 )
